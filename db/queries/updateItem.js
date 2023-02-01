27f5b74ebf84eb7e94622225aec507dcc08ddf69
egesenli <egesenli@gmail.com>
2023-02-01 17:29:45 -0500

    Refactor updateItem function

--- db/queries/updateItem.js
+++ db/queries/updateItem.js
@@ -2,18 +2,18 @@ const db = require('../connection');
 
 const updateItem = (itemID, category) => {
   console.log('Item ID = ' + itemID + ' Category: ' + category);
-  //create update item query
-  const query = `UPDATE items SET category_id = (SELECT id FROM categories WHERE category_name = $1) WHERE id = $2;`;
-  
-  const values = [category.category_name, itemID];
+
+  const query = `UPDATE items SET category_id = (SELECT id FROM categories WHERE category_name = $1) WHERE id = $2`;
+  const values = [category, itemID];
 
   return db.query(query, values)
     .then(res => {
-      return res;
+      console.log(res.rows);
+      return res.rows;
     })
     .catch(err => {
       console.error(err);
-      return err;
+      throw err;
     });
 };
 
