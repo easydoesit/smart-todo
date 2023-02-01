f37afcf95c11a2555bfb4003702bff808f881ba0
egesenli <egesenli@gmail.com>
2023-02-01 01:29:05 -0500

    Refactor toWatch function

--- db/queries/toWatch.js
+++ db/queries/toWatch.js
@@ -1,10 +1,17 @@
 const db = require('../connection');
 
 const toWatch = () => {
-  //query toWatch catagory
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
+  const query = `SELECT * FROM items WHERE category_id = (SELECT id FROM categories WHERE category_name = $1);`;
+
+  const values = ['Film & Series'];
+
+  return db.query(query, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      console.error(err);
+      return err;
     });
 };
 
