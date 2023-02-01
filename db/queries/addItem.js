2d6f36abb5409caf6e06195d61cffbc250f8b13b
egesenli <egesenli@gmail.com>
2023-02-01 01:26:50 -0500

    Refactor addItem function

--- db/queries/addItem.js
+++ db/queries/addItem.js
@@ -2,10 +2,24 @@ const db = require('../connection');
 
 const addItem = (item, category) => {
   console.log('Item = ' + item + ' Category: ' + category);
-  //create add item query
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
+
+  const query = `
+  INSERT INTO items (category_id, user_id, item_name, due_date, priority)
+  SELECT categories.id, users.id, $1, $2, $3
+  FROM categories
+  JOIN users ON users.email = $4
+  WHERE categories.category_name = $5;
+`;
+
+  const values = [item.item_name, item.due_date, item.priority, item.email, category.category_name];
+
+  return db.query(query, values)
+    .then(res => {
+      return res;
+    })
+    .catch(err => {
+      console.error(err);
+      return err;
     });
 };
 
