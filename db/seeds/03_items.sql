d663a5d7fc0c63eb186df5e1162ce23830d78ee9
egesenli <egesenli@gmail.com>
2023-02-06 14:37:53 -0500

    Create new items data seeds

--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -10,3 +10,27 @@ INSERT INTO items (category_id, user_id, item_name, created_date, due_date, comp
 INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status) VALUES (4, 4, 'New camera lens for photography', '2023-03-20', '2023-04-01', '2023-04-03', 2, FALSE);
 
 INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status) VALUES (1, 3, 'The Godfather', '2023-03-01', '2023-03-10', NULL, 5, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (1, 1, 'Interstellar', '2023-03-05', '2023-03-15', '2023-03-12', 4, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (1, 2, 'Inception', '2023-03-15', '2023-03-25', '2023-03-23', 3, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (3, 1, '1984', '2023-03-10', '2023-03-20', NULL, 2, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (3, 3, 'The Catcher In The Rye', '2023-03-20', '2023-03-30', '2023-03-27', 5, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (3, 1, 'Brave New World', '2023-03-15', '2023-03-25', NULL, 4, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (1, 1, 'Avatar', '2023-03-01', '2023-03-05', '2023-03-03', 3, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (1, 1, 'Memento', '2023-03-05', '2023-03-15', '2023-03-10', 2, TRUE);
+
+INSERT INTO items (category_id, user_id, item_name, created_date, due_date, completed_date, priority, item_status)
+VALUES (3, 1, 'The Night Circus', '2023-03-10', '2023-03-20', '2023-03-15', 5, TRUE);
