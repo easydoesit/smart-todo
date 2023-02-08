5675488cb72a53bf534572cca46dfea378f0d0f0
egesenli <egesenli@gmail.com>
2023-02-07 22:02:02 -0500

    Refactor getItems function query

--- db/queries/items.js
+++ db/queries/items.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 const getItems = () => {
   //Get all active items
-  const query = `SELECT * FROM items WHERE item_status = true;`;
+  const query = `SELECT * FROM items WHERE item_status = true ORDER BY priority ASC;`;
 
   return db.query(query)
     .then(res => {
