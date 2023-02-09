64bcb810a2bb0a64b69054f6600aede90f3a6b14
egesenli <egesenli@gmail.com>
2023-02-08 21:46:03 -0500

    Add priority data while creating new item

--- db/queries/addItem.js
+++ db/queries/addItem.js
@@ -1,13 +1,13 @@
 const db = require('../connection');
 
-const addItem = (userID, item, category) => {
-  console.log('Item = ' + item + ' User ID = ' + userID + ' Category = ' + category);
+const addItem = (userID, item, category, priority) => {
+  console.log('Item = ' + item + ' User ID = ' + userID + ' Category = ' + category + ' Priority = ' + priority);
   //write query to add item to database
-  const query = `INSERT INTO items (user_id, category_id, item_name)
+  const query = `INSERT INTO items (user_id, category_id, item_name, priority)
 
-  VALUES ($1, (SELECT id FROM categories WHERE category_name = $2), $3) RETURNING *`;
+  VALUES ($1, (SELECT id FROM categories WHERE category_name = $2), $3, $4) RETURNING *`;
 
-  const values = [userID, category, item];
+  const values = [userID, category, item, priority];
 
   return db.query(query, values)
     .then(res => {
